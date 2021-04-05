16eaf4c15117d31b82367d3b941cd7cf4fd7e421
Érika Marcia <erikamarciapereira@outlook.com>
2021-04-05 00:04:01 -0300

    Revert "Revert "Implementação service login""
    This reverts commit cecacee65a056422080e67b890bc4f8d6261d38e.

--- Codigo/front/src/app/login/login.component.ts
+++ Codigo/front/src/app/login/login.component.ts
@@ -1,4 +1,7 @@
+import { LoginService } from './../services/login.service';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
+  hide = true;
 
-  constructor() { }
+  constructor(
+    private _formBuilder: FormBuilder,
+    private _service: LoginService,
+    private _snackBar: MatSnackBar
+  ) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(){
+    this.form = this._formBuilder.group({
+     email : [null, [Validators.required, Validators.email]],
+     senha: [null, Validators.required]
+  
+   })}
+
+   login(){
+     let email = this.form.get("email")?.value;
+     let senha = this.form.get("senha")?.value;
+     let corpo = {emailAddress: email, password: senha};
+     let corpoJson = JSON.stringify(corpo);
+     if (this.form.valid){
+       this._service.login(corpoJson);
+       
+     }
+   }
+   
 }
