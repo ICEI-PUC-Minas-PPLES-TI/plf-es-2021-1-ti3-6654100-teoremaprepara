4498af8ce923d3ca6f276177b25a70aeb6125d08
Italo Lelis <italohdc@gmail.com>
2021-04-26 18:22:00 -0300

    Change frontend url on staging

--- Codigo/backend/config/env/staging.js
+++ Codigo/backend/config/env/staging.js
@@ -90,6 +90,7 @@ module.exports = Object.assign({}, PRODUCTION_CONFIG, {
   custom: Object.assign({}, PRODUCTION_CONFIG.custom, {
 
     baseUrl: 'https://teorema-prepara.herokuapp.com',
+    frontUrl: 'https://teoremapreparatorio.netlify.app',
     //--------------------------------------------------------------------------
     // /\  Hard-code the base URL where your staging environment is hosted.
     // ||  (or use system env var: `sails_custom__baseUrl`)
