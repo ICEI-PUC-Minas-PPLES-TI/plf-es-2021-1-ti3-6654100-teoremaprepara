62523aab6b6719ee29d88275b62825d91d27b843
Érika Marcia <erikamarciapereira@outlook.com>
2021-05-17 12:08:39 -0300

    Alteração menu e header dinamicos

--- Codigo/front/src/app/admin/painel-simulados/painel-simulados.module.ts
+++ Codigo/front/src/app/admin/painel-simulados/painel-simulados.module.ts
@@ -1,12 +1,20 @@
+import { PainelSimuladosComponent } from './painel-simulados.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
-
+const routes = [
+  {
+      path: '',
+      component: PainelSimuladosComponent
+  }
+];
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
   ]
 })
 export class PainelSimuladosModule { }
