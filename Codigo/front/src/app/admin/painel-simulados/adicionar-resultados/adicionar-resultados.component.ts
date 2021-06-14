c8d69e09d9e2759b97a04087269a4830827b26c6
Érika Marcia <erikamarciapereira@outlook.com>
2021-06-14 16:33:16 -0300

    Cadastrar simulados

--- Codigo/front/src/app/admin/painel-simulados/adicionar-resultados/adicionar-resultados.component.ts
+++ Codigo/front/src/app/admin/painel-simulados/adicionar-resultados/adicionar-resultados.component.ts
@@ -1,8 +1,10 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import * as XLSX from 'xlsx';
+import { PainelSimuladosService } from '../../services/painel-simulados.service';
 
-export interface Curso {
+export interface Simulado {
   name: String;
   id: String;
 }
@@ -15,23 +17,57 @@ export interface Curso {
 export class AdicionarResultadosComponent implements OnInit {
 
   form: FormGroup;
-  
+  ResultadoSimulado: [][];
+  nomeArquivoSelecionado: string = "";
   constructor(
+    private _service: PainelSimuladosService,
     public dialogRef: MatDialogRef<AdicionarResultadosComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Curso,
+    @Inject(MAT_DIALOG_DATA) public data: Simulado,
     private _formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
   }
+
+  onFileChange(event: any){
+    const arquivo: DataTransfer = <DataTransfer>(event.target);
+    
+    if (arquivo.files.length !== 1) throw new Error('Multiplo arquivo')
+    const reader: FileReader = new FileReader();
+    this.nomeArquivoSelecionado = event.target.files[0].name;
+
+    reader.onload = (e: any) => {
+      const bstr: string = e.target.result;
+
+      const wb: XLSX.WorkBook = XLSX.read(bstr, { type: 'binary' });
+
+      const wsname : string = wb.SheetNames[0];
+
+      const ws: XLSX.WorkSheet = wb.Sheets[wsname];
+
+      this.ResultadoSimulado = (XLSX.utils.sheet_to_json(ws, { header: 1 }));
+
+    };
+
+    reader.readAsBinaryString(arquivo.files[0]);
+  }
+
+  confirmar(){
+    console.log(this.ResultadoSimulado);
+    const userJSON = this.ResultadoSimulado;
+    this._service.inserirNotas(this.data.id);
+    this.close();
+    location.reload();
+  }
+
   initForm(){
     this.form = this._formBuilder.group({
       nome : [null],
       descricao: [null],
    })}
+
    close(){
     this.dialogRef.close();
-    
    }
 
 }
